83fb5ff219d6b101c13c5a0c086fea0cee1ed687
HP <HP@DESKTOP-UMI7DIG>
2023-06-20 16:11:27 +0530

    typecasting

---
+++ typecasting/src/typecasting/Typecasting1.java
@@ -0,0 +1,17 @@
+package typecasting;
+
+
+
+public class Typecasting1{
+
+	public static void main(String args[])
+	{
+		 // create int type variable
+	    int num = 10;
+	    System.out.println("The integer value: " + num);
+
+	    // convert into double type
+	    double data = num;
+	    System.out.println("The double value: " + data);
+	}
+	}
---
+++ typecasting/src/typecasting/Typecasting2.java
@@ -0,0 +1,18 @@
+package typecasting;
+
+
+
+public class Typecasting2 {
+
+	public static void main(String[] args) {
+		// create double type variable
+	    double num = 11.99;
+	    System.out.println("The double value: " + num);
+
+	    // convert into int type
+	    int data = (int)num;
+	    System.out.println("The integer value: " + data);
+
+	}
+
+}
